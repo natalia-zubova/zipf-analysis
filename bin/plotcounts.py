8d8dc863592622960c169590602dcb069099656e
Natalia Zubova <natalia.s.zubova@gmail.com>
2024-04-16 13:28:08 +0100

    automate analysis

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -2,16 +2,34 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
+def main(args):
+    df = pd.read_csv(args.infile, header=None,
+                    names=('word', 'word_frequency'))
+    df['rank'] = df['word_frequency'].rank(ascending=False,
+                                        method='max')
+    df['inverse_rank'] = 1 / df['rank']
+    ax = df.plot.scatter(x='word_frequency',
+                        y='inverse_rank',
+                        figsize=[12, 6],
+                        grid=True,
+                        xlim=args.xlim)
+    plt.savefig(args.outfile)
 
-df = pd.read_csv('results/frankenstein.csv', header=None,
-                 names=('word', 'word_frequency'))
-df['rank'] = df['word_frequency'].rank(ascending=False,
-                                       method='max')
-df['inverse_rank'] = 1 / df['rank']
-ax = df.plot.scatter(x='word_frequency',
-                     y='inverse_rank',
-                     figsize=[12, 6],
-                     grid=True,
-                     xlim=None)
-plt.show()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=(
+        "Count the occurrences of all words in a text "
+        "and write them to a CSV-file."
+    ))
+    parser.add_argument('infile', type=argparse.FileType('r'),
+                        nargs='?', default='-',
+                        help='Input file name')
+    parser.add_argument('--xlim', type=float, nargs=2,
+                        metavar=('XMIN', 'XMAX'),
+                        default=None, help='X-axis limits')
+    parser.add_argument('--outfile', type=str,
+                        default='plotcounts.png',
+                        help='Output image file name')
+    args = parser.parse_args()
+    main(args)
